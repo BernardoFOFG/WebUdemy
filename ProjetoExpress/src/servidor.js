106cc6bd65a6bfc41b63ef5435ebde81cc6c89cc
BernardoFOFG <bernardofofg@gmail.com>
2023-01-04 01:30:59 -0300

    Aula 02: Projeto Express

--- ProjetoExpress/src/servidor.js
+++ ProjetoExpress/src/servidor.js
@@ -2,11 +2,23 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
+const bancoDeDados = require("./bancoDeDados");
 
 app.get("/produtos", (req, res, next) => {
-  res.send({ nome: "Notebook", preco: 123.45 }); // res.send = Objeto é convertido para JSON automaticamente
+  res.send(bancoDeDados.getProdutos()); // res.send = Objeto é convertido para JSON automaticamente
 });
 
+app.get("/produtos/:id", (req, res, next) => {
+  res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post("/produtos", (req, res, next) => {
+  const produto = bancoDeDados.salvarProduto({
+    nome: req.body.name,
+    preço: req.body.preco,
+  });
+  res.send(produto);  // res.send = Objeto é convertido para JSON automaticamente
+});
 app.listen(porta, () => {
   console.log(`Servidor executando na porta ${porta}`);
 });
