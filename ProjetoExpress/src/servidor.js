28dc4aef90c562b2e8a41d59b17c353d4bd8c774
BernardoFOFG <bernardofofg@gmail.com>
2023-01-04 15:05:15 -0300

    Aula 03 - Projeto Express

--- ProjetoExpress/src/servidor.js
+++ ProjetoExpress/src/servidor.js
@@ -2,22 +2,40 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const bancoDeDados = require("./bancoDeDados");
 
-app.get("/produtos", (req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/produtos", (req, res) => {
   res.send(bancoDeDados.getProdutos()); // res.send = Objeto é convertido para JSON automaticamente
 });
 
-app.get("/produtos/:id", (req, res, next) => {
+app.get("/produtos/:id", (req, res) => {
   res.send(bancoDeDados.getProduto(req.params.id));
 });
 
-app.post("/produtos", (req, res, next) => {
+app.post("/produtos", (req, res) => {
   const produto = bancoDeDados.salvarProduto({
-    nome: req.body.name,
-    preço: req.body.preco,
+    nome: req.body.nome,
+    preco: req.body.preco,
   });
-  res.send(produto);  // res.send = Objeto é convertido para JSON automaticamente
+
+  res.send(produto); // res.send = Objeto é convertido para JSON automaticamente
+});
+
+app.put("/produtos/:id", (req, res) => {
+  const produto = bancoDeDados.salvarProduto({
+    id: req.params.id,
+    nome: req.body.nome,
+    preco: req.body.preco,
+  });
+
+  res.send(produto); // res.send = Objeto é convertido para JSON automaticamente
+});
+app.delete("/produtos/:id", (req, res) => {
+  const produto = bancoDeDados.excluirProduto(req.params.id);
+  res.send(produto); // res.send = Objeto é convertido para JSON automaticamente
 });
 app.listen(porta, () => {
   console.log(`Servidor executando na porta ${porta}`);
