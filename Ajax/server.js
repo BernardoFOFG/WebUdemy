44a9400ab90a0378160e63056eb44ececb38f6ce
BernardoFOFG <bernardofofg@gmail.com>
2023-02-14 12:23:37 -0300

    XML HTTP Request 1

--- Ajax/server.js
+++ Ajax/server.js
@@ -6,5 +6,4 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true })); // pegando submit de um formulário e transformando em objeto
 app.use(bodyParser.json()); // transforma em objeto
 
-app.get("/teste", (req, res) => res.send("Deu tudo certo!"));
-app.listeners(8080, () => console.log("Servidor funcionando!"))
+app.listen(8080, () => console.log("Servidor funcionando!"));
