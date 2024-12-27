5a256da5119ef872fe65c1729927a4e1060a08f7
BernardoFOFG <bernardofofg@gmail.com>
2024-12-27 08:03:21 -0300

    fix: tabela de empresas

--- Banco-Relacional/crud-sql/create-table-company.sql
+++ Banco-Relacional/crud-sql/create-table-company.sql
@@ -1,25 +1,18 @@
-CREATE TABLE
-IF NOT EXISTS empresas
-(
+CREATE TABLE IF NOT EXISTS empresas (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  nome VARCHAR
-(255) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
   cnpj INT UNSIGNED,
-  PRIMARY KEY
-(id),
-  UNIQUE KEY
-(cnpj),
+  PRIMARY KEY (id),
+  UNIQUE KEY (cnpj),
 
 )
 
 
-CREATE TABLE
-IF NOT EXISTS empresas_unidades
-(
+-- Depois verifica isso: Integer display width is deprecated and will be removed in a future release.
+
+CREATE TABLE IF NOT EXISTS empresas_unidades (
   empresa_id INT UNSIGNED NOT NULL,
   cidade_id INT UNSIGNED NOT NULL,
-  sede TINYINT
-(1) NOT NULL,
-  PRIMARY KEY
-(empresa_id, cidade_id)
+  sede TINYINT(1) NOT NULL,
+  PRIMARY KEY (empresa_id, cidade_id)
 )
