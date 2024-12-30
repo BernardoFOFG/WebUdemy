a236ef78a9a2a37964e8b1101b4b1f8226f65f89
BernardoFOFG <bernardofofg@gmail.com>
2024-12-30 00:51:17 -0300

    add: criando primeira rota com express

--- Express/index.js
+++ Express/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use("/", (req, res) => {  
+	// app.use ele aceita requisições de qualquer tipo.
+	// app.get ele aceita apenas requisições do tipo GET.
+	// app.post ele aceita apenas requisições do tipo POST.
+	// e assim sucessivamente.
+	res.send("Hello World");
+});
+
 app.listen(3000, () => {
 	console.log("Servidor Iniciado");
 });
