42c5f5189e9c9c5824ed508bcb2573a39daf5102
BernardoFOFG <bernardofofg@gmail.com>
2024-12-30 00:57:30 -0300

    add: introdução a tipos de respostas de requisições

--- Express/index.js
+++ Express/index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {  
+app.use("/", (req, res) => {
 	// app.use ele aceita requisições de qualquer tipo.
 	// app.get ele aceita apenas requisições do tipo GET.
 	// app.post ele aceita apenas requisições do tipo POST.
 	// e assim sucessivamente.
 	res.send("Hello World");
+});
+
+// Retornando um objeto JSON diretamente
+app.get("/json", (req, res) => {
+	res.json({ message: "Hello World" });
+});
+
+// Retornando um objeto JSON, porém descritivo com suas propriedades
+app.get("/json2", (req, res) => {
+	res.json({
+		data: [
+			{
+				name: "João",
+				age: 20,
+			},
+			{
+				name: "Maria",
+				age: 25,
+			},
+		],
+		status: 200,
+		res: "OK",
+	});
 });
 
 app.listen(3000, () => {
