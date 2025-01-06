3cff8014bdcf72cfeaff2ebcefee4c04f1e850f4
BernardoFofg-NE <bernardofofg@gmail.com>
2025-01-06 15:21:09 -0300

    add: protegendo apis com passport

--- Projeto-Final/back-end/config/routes.js
+++ Projeto-Final/back-end/config/routes.js
@@ -1,31 +1,53 @@
 module.exports = (app) => {
-	app.post("/signUp", app.api.user.save);
-	app.post("/signIn", app.api.auth.signIn);
-	app.post("/validateToken", app.api.auth.validateToken);
+  app.post("/signUp", app.api.user.save);
+  app.post("/signIn", app.api.auth.signIn);
+  app.post("/validateToken", app.api.auth.validateToken);
 
-	app.route("/users").post(app.api.user.save).get(app.api.user.get);
-	app.route("/users/:id").put(app.api.user.save).get(app.api.user.getUserById);
+  app
+    .route("/users")
+    .all(app.config.passport.authenticate())
+    .post(app.api.user.save)
+    .get(app.api.user.get);
 
-	app
-		.route("/categories")
-		.get(app.api.category.get) // Obtém todas as categorias
-		.post(app.api.category.save); // Cria uma nova categoria
+  app
+    .route("/users/:id")
+    .all(app.config.passport.authenticate())
+    .put(app.api.user.save)
+    .get(app.api.user.getUserById);
 
-	// Cuidado com a ordem! Tem qeu vir antes da /categories/:id
-	app.route("/categories").get(app.api.category.getTree);
+  app
+    .route("/categories")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.get) // Obtém todas as categorias
+    .post(app.api.category.save); // Cria uma nova categoria
 
-	app
-		.route("/categories/:id")
-		.get(app.api.category.getById) // Obtém uma categoria pelo ID
-		.put(app.api.category.save) // Atualiza uma categoria pelo ID
-		.delete(app.api.category.remove); // Remove uma categoria pelo ID
+  // Cuidado com a ordem! Tem qeu vir antes da /categories/:id
+  app
+    .route("/categories")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getTree);
 
-	app.route("/articles").post(app.api.article.save).get(app.api.article.get);
-	app
-		.route("/articles/:id")
-		.put(app.api.article.save)
-		.delete(app.api.article.remove)
-		.get(app.api.article.getById);
+  app
+    .route("/categories/:id")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getById) // Obtém uma categoria pelo ID
+    .put(app.api.category.save) // Atualiza uma categoria pelo ID
+    .delete(app.api.category.remove); // Remove uma categoria pelo ID
 
-	app.route("/categories/:id/articles").get(app.api.article.getByCategory);
+  app
+    .route("/articles")
+    .all(app.config.passport.authenticate())
+    .post(app.api.article.save)
+    .get(app.api.article.get);
+  app
+    .route("/articles/:id")
+    .all(app.config.passport.authenticate())
+    .put(app.api.article.save)
+    .delete(app.api.article.remove)
+    .get(app.api.article.getById);
+
+  app
+    .route("/categories/:id/articles")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getByCategory);
 };
--- Projeto-Final/back-end/index.js
+++ Projeto-Final/back-end/index.js
@@ -5,8 +5,9 @@ const db = require("./config/db");
 app.db = db; // O DB, é um extensão do Knex que faz a conexão com o banco de dados
 
 consign() // Gerenciador de dependências
+  .include("./config/passport.js")
   .then("./config/middlewares.js")
-  .then('./api/validation.js')
+  .then("./api/validation.js")
   .then("./api")
   .then("./config/routes.js")
   .into(app);
