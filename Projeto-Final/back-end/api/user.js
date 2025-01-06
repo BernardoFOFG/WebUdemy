35149a9b2156f2b7fbf99b82ed67a366a20b63bf
BernardoFofg-NE <bernardofofg@gmail.com>
2025-01-06 14:14:47 -0300

    add: api de autenticação

--- Projeto-Final/back-end/api/user.js
+++ Projeto-Final/back-end/api/user.js
@@ -1,79 +1,79 @@
 const bycrpy = require("bcrypt");
 
 module.exports = (app) => {
-  const { existOrError, notExistOrError, equalsOrError } = app.api.validation;
+	const { existOrError, notExistOrError, equalsOrError } = app.api.validation;
 
-  const encryptPassword = (password) => {
-    // Encriptografando a senha
-    const salt = bycrpy.genSaltSync(10);
-    return bycrpy.hashSync(password, salt);
-  };
-  const save = async (req, res) => {
-    const user = { ...req.body };
-    if (req.params.id) user.id = req.params.id;
+	const encryptPassword = (password) => {
+		// Encriptografando a senha
+		const salt = bycrpy.genSaltSync(10);
+		return bycrpy.hashSync(password, salt);
+	};
+	const save = async (req, res) => {
+		const user = { ...req.body };
+		if (req.params.id) user.id = req.params.id;
 
-    try {
-      existOrError(user.name, "Nome não informado");
-      existOrError(user.email, "E-mail não informado");
-      existOrError(user.password, "Senha não informada");
-      existOrError(user.confirmPassword, "Confirmação de senha inválida");
-      equalsOrError(user.password, user.confirmPassword, "Senhas não conferem");
+		try {
+			existOrError(user.name, "Nome não informado");
+			existOrError(user.email, "E-mail não informado");
+			existOrError(user.password, "Senha não informada");
+			existOrError(user.confirmPassword, "Confirmação de senha inválida");
+			equalsOrError(user.password, user.confirmPassword, "Senhas não conferem");
 
-      const userFromDB = await app
-        .db("users")
-        .where({ email: user.email })
-        .first();
-      if (!user.id) {
-        notExistOrError(userFromDB, "Usuário já cadastrado");
-      }
-    } catch (error) {
-      return res.status(400).send(error); // Código do erro 400: Bad Request, junto com a mensagem de erro
-    }
+			const userFromDB = await app
+				.db("users")
+				.where({ email: user.email })
+				.first();
+			if (!user.id) {
+				notExistOrError(userFromDB, "Usuário já cadastrado");
+			}
+		} catch (error) {
+			return res.status(400).send(error); // Código do erro 400: Bad Request, junto com a mensagem de erro
+		}
 
-    user.password = encryptPassword(user.password);
-    delete user.confirmPassword;
+		user.password = encryptPassword(user.password);
+		delete user.confirmPassword;
 
-    if (user.id) {
-      app
-        .db("users")
-        .updated(user)
-        .where({ id: user.id })
-        .then((_) => res.status(204).send())
-        .catch((err) => res.status(500).send(err));
-    } else {
-      app
-        .db("users")
-        .insert(user)
-        .then((_) => res.status(204).send())
-        .catch((err) => res.status(500).send(err));
-    }
-  };
+		if (user.id) {
+			app
+				.db("users")
+				.updated(user)
+				.where({ id: user.id })
+				.then((_) => res.status(204).send())
+				.catch((err) => res.status(500).send(err));
+		} else {
+			app
+				.db("users")
+				.insert(user)
+				.then((_) => res.status(204).send())
+				.catch((err) => res.status(500).send(err));
+		}
+	};
 
-  const get = (req, res) => {
-    // Função para listar todos os usuários
-    app
-      .db("users")
-      .select("id", "name", "email", "admin")
-      .then((users) => res.json(users))
-      .catch((err) => res.status(500).send(err));
-  };
+	const get = (req, res) => {
+		// Função para listar todos os usuários
+		app
+			.db("users")
+			.select("id", "name", "email", "admin")
+			.then((users) => res.json(users))
+			.catch((err) => res.status(500).send(err));
+	};
 
-  const getUserById = (req, res) => {
-    app
-      .db("users")
-      .select("id", "name", "email", "admin")
-      .where({ id: req.params.id })
-      .first()
-      .then((user) => {
-        try {
-          existOrError(user, "Usuário não encontrado.");
-          res.json(user);
-        } catch (msg) {
-          res.status(404).send(msg); // Código 404 para "Not Found"
-        }
-      })
-      .catch((err) => res.status(500).send(err));
-  };
+	const getUserById = (req, res) => {
+		app
+			.db("users")
+			.select("id", "name", "email", "admin")
+			.where({ id: req.params.id })
+			.first()
+			.then((user) => {
+				try {
+					existOrError(user, "Usuário não encontrado.");
+					res.json(user);
+				} catch (msg) {
+					res.status(404).send(msg); // Código 404 para "Not Found"
+				}
+			})
+			.catch((err) => res.status(500).send(err));
+	};
 
-  return { save, get, getUserById };
+	return { save, get, getUserById };
 };
--- Projeto-Final/back-end/config/routes.js
+++ Projeto-Final/back-end/config/routes.js
@@ -1,27 +1,31 @@
 module.exports = (app) => {
-  app.route("/users").post(app.api.user.save).get(app.api.user.get);
-  app.route("/users/:id").put(app.api.user.save).get(app.api.user.getUserById);
+	app.post("/signUp", app.api.user.save);
+	app.post("/signIn", app.api.auth.signIn);
+	app.post("/validateToken", app.api.auth.validateToken);
 
-  app
-    .route("/categories")
-    .get(app.api.category.get) // Obtém todas as categorias
-    .post(app.api.category.save); // Cria uma nova categoria
+	app.route("/users").post(app.api.user.save).get(app.api.user.get);
+	app.route("/users/:id").put(app.api.user.save).get(app.api.user.getUserById);
 
-  // Cuidado com a ordem! Tem qeu vir antes da /categories/:id
-  app.route("/categories").get(app.api.category.getTree);
+	app
+		.route("/categories")
+		.get(app.api.category.get) // Obtém todas as categorias
+		.post(app.api.category.save); // Cria uma nova categoria
 
-  app
-    .route("/categories/:id")
-    .get(app.api.category.getById) // Obtém uma categoria pelo ID
-    .put(app.api.category.save) // Atualiza uma categoria pelo ID
-    .delete(app.api.category.remove); // Remove uma categoria pelo ID
+	// Cuidado com a ordem! Tem qeu vir antes da /categories/:id
+	app.route("/categories").get(app.api.category.getTree);
 
-  app.route("/articles").post(app.api.article.save).get(app.api.article.get);
-  app
-    .route("/articles/:id")
-    .put(app.api.article.save)
-    .delete(app.api.article.remove)
-    .get(app.api.article.getById);
+	app
+		.route("/categories/:id")
+		.get(app.api.category.getById) // Obtém uma categoria pelo ID
+		.put(app.api.category.save) // Atualiza uma categoria pelo ID
+		.delete(app.api.category.remove); // Remove uma categoria pelo ID
 
-  app.route("/categories/:id/articles").get(app.api.article.getByCategory);
+	app.route("/articles").post(app.api.article.save).get(app.api.article.get);
+	app
+		.route("/articles/:id")
+		.put(app.api.article.save)
+		.delete(app.api.article.remove)
+		.get(app.api.article.getById);
+
+	app.route("/categories/:id/articles").get(app.api.article.getByCategory);
 };
