ae65525f1ed082f776441d4d5c7544f4e4e0f00f
BernardoFofg-NE <bernardofofg@gmail.com>
2025-01-07 14:49:20 -0300

    add: componente de titulo das páginas

---
+++ Projeto-Final/front-end/src/components/template/PageTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-title">
+    <h1><i v-if="icon" :class="icon"></i> {{ title }}</h1>
+    <h2>{{ subtitle }}</h2>
+    <hr>
+  </div>
+</template>
+<script>
+export default {
+  name: "PageTitle",
+  props: ['icon', 'title', 'subtitle']
+}
+</script>
+<style>
+  .page-title h1 {
+    margin: 0px;
+  }
+
+  .page-title h2 {
+    color: #777;
+    margin-top: 5px;
+    font-size: 1.3rem;
+  }
+</style>
