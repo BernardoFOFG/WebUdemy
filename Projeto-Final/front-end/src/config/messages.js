41e9a5dff6a7ea31197c8fa66c60a3a6f5ea24eb
BernardoFOFG <bernardofofg@gmail.com>
2025-01-07 23:24:34 -0300

    add: finalizando as funcionalidades de usuários

--- Projeto-Final/front-end/src/config/messages.js
+++ Projeto-Final/front-end/src/config/messages.js
@@ -6,14 +6,14 @@ Vue.use(Toasted, {
   duration: 3000,
 });
 
-Vue.Toasted.register(
+Vue.toasted.register(
   "defaultSuccess",
-  (payload) => (!payload.msg ? "Operação realizada com sucesso!" : payload.msg),
+  (payload) => (!payload.msg ? "Operação realidada com sucesso!" : payload.msg),
   { type: "success", icon: "check" }
 );
 
-Vue.Toasted.register(
+Vue.toasted.register(
   "defaultError",
-  (payload) => (!payload.msg ? "Ops... Erro inesperado" : payload.msg),
+  (payload) => (!payload.msg ? "Oops.. Erro inesperado." : payload.msg),
   { type: "error", icon: "times" }
 );
